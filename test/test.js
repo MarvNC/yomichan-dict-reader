792e854f0e90cee43d40444e816651c2e3093f7a
MarvNC <rotrobmin@gmail.com>
2022-10-20 15:02:22 -0700

    initial commit

---
+++ test/test.js
@@ -0,0 +1,22 @@
+const Yomichan = require('../yomichan');
+
+const yomichan = new Yomichan();
+
+(async function () {
+  const JMDictPath = './test/[Bilingual] JMdict (English) Alternate.zip';
+  await yomichan.readDictionary(JMDictPath);
+  const 家Readings = yomichan.getReadingsForTerm('家');
+  console.log(家Readings);
+  const よいTerms = yomichan.getTermsForReading('よい');
+  console.log(よいTerms);
+  for (const entry of よいTerms) {
+    console.log(yomichan.getDefinitionsForTermReading(entry, 'よい'));
+  }
+  const allJMDict = await yomichan.getAllEntriesFromDict(JMDictPath);
+  console.log('Keys: ', Object.keys(allJMDict).length);
+  const allJMDictTerms = await yomichan.getAllTermsInDict(JMDictPath);
+  console.log('Terms: ', allJMDictTerms.size);
+  console.log('JMDict contains 家: ', yomichan.dictContains(JMDictPath, '家'));
+  console.log('JMDict contains DASDAS: ', yomichan.dictContains(JMDictPath, 'DASDAS'));
+  console.log('Finished');
+})();
---
+++ yomichan.js
@@ -0,0 +1,166 @@
+const fs = require('fs');
+const jszip = require('jszip');
+
+const termBankRegex = /term_bank_\d+\.json/;
+
+const Yomichan = class {
+  allTermReadingPairsData = {};
+  allKeys = {};
+  allReadings = {};
+  // basically allTermReadingPairsData but with a dict key separating out entries for each dict
+  allDicts = {};
+  constructor(path = null) {
+    if (path) {
+      this.readDictionary(path);
+    }
+  }
+
+  /**
+   * Reads a zip file dictionary of the Yomichan format
+   * @param {string} dictname
+   */
+  async readDictionary(dictname) {
+    console.log('Reading dictionary: ', dictname);
+    const zipFile = await fs.promises.readFile(dictname);
+    const zip = await jszip.loadAsync(zipFile);
+
+    this.allDicts[dictname] = {};
+
+    let fileCount = 0;
+    let entryCount = 0;
+    for (const filename of Object.keys(zip.files)) {
+      if (termBankRegex.test(filename)) {
+        console.log(`Reading ${filename} from ${dictname}`);
+        const file = await zip.file(filename).async('string');
+        const json = JSON.parse(file);
+        for (const entry of json) {
+          const [term, reading, tags, deinflectors, popularity, definitions, sequence, bigTags] =
+            entry;
+
+          const thisEntry = {
+            term,
+            reading,
+            tags,
+            deinflectors,
+            popularity,
+            definitions,
+            sequence,
+            bigTags,
+            dict: dictname,
+          };
+
+          // add entry data
+          if (!this.allTermReadingPairsData[[term, reading]]) {
+            this.allTermReadingPairsData[[term, reading]] = [];
+          }
+          this.allTermReadingPairsData[[term, reading]].push(thisEntry);
+
+          // add to this dict
+          if (!this.allDicts[dictname][[term, reading]]) {
+            this.allDicts[dictname][[term, reading]] = [];
+          }
+          this.allDicts[dictname][[term, reading]].push(thisEntry);
+
+          // add readings lookup
+          if (!this.allKeys[term]) {
+            this.allKeys[term] = [];
+          }
+          if (!this.allKeys[term].includes(reading)) {
+            this.allKeys[term].push(reading);
+          }
+
+          // add term lookup from reading
+          if (!this.allReadings[reading]) {
+            this.allReadings[reading] = [];
+          }
+          if (!this.allReadings[reading].includes(term)) {
+            this.allReadings[reading].push(term);
+          }
+          entryCount++;
+        }
+        fileCount++;
+      }
+    }
+    console.log(`Read ${fileCount} files with ${entryCount} entries from ${dictname}`);
+  }
+
+  /**
+   * Reads multiple dictionaries
+   * @param {Array.<string>} dictionaries
+   */
+  async readDictionaries(dictionaries) {
+    for (const dict of dictionaries) {
+      await this.readDictionary(dict);
+    }
+  }
+
+  /**
+   * Given a dictionary name, gets every single entry from that dictionary.
+   * @param {string} filename
+   */
+  async getAllEntriesFromDict(filename) {
+    if (!this.allDicts[filename]) {
+      await readDictionary(filename);
+    }
+    return this.allDicts[filename];
+  }
+
+  /**
+   * Given a dictionary name, gets every single term from that dictionary as a set.
+   * @param {string} filename
+   * @returns {Set.<string>}
+   */
+  async getAllTermsInDict(filename) {
+    const allTermreadingPairs = Object.keys(await this.getAllEntriesFromDict(filename));
+    return new Set(allTermreadingPairs.map((entry) => entry.split(',')[0]));
+  }
+
+  /**
+   * Checks if a dictionary contains a term.
+   * @param {string} filename filename of the dictionary to search
+   * @param {string} term the term to check for existence of
+   */
+  dictContains(filename, term) {
+    const readings = this.getReadingsForTerm(term);
+    for (const reading of readings) {
+      const definitions = this.getDefinitionsForTermReading(term, reading);
+      if (definitions.some((definition) => definition.dict == filename)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Gets readings of a kanji term.
+   * @param {string} term
+   */
+  getReadingsForTerm(term) {
+    return this.allKeys[term] ?? [];
+  }
+
+  /**
+   * Gets the terms for a reading
+   * @param {string} reading
+   */
+  getTermsForReading(reading) {
+    return this.allReadings[reading] ?? [];
+  }
+
+  /**
+   * Gets the definitions for a term reading pair.
+   * @param {string} term The term in kanji, or hiragana/katakana if no kanji exists for the term.
+   * @param {string} reading The reading in hiragana.
+   * @returns {Array.<Object>} A list of definitions or an empty array.
+   */
+  getDefinitionsForTermReading(term, reading = '') {
+    // if term contains comma and reading is empty, separate into term reading
+    if (term.includes(',') && reading == '') {
+      [term, reading] = term.split(',');
+    }
+    let data = this.allTermReadingPairsData[[term, reading]];
+    return data ?? [];
+  }
+};
+
+module.exports = Yomichan;
